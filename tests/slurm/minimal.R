f943238a93479f4dbdd1e3c87589f4888e2a4821
wlandau <will.landau@gmail.com>
2023-05-04 14:05:09 -0400

    Add test scripts for actual SLURM clusters

---
+++ tests/slurm/minimal.R
@@ -0,0 +1,18 @@
+library(crew.cluster)
+library(testthat)
+controller <- crew_controller_slurm(
+  name = "my_workflow",
+  workers = 1L,
+  seconds_idle = 300,
+  slurm_lines = paste0("module load R/", getRversion()),
+  verbose = TRUE
+)
+controller$start()
+controller$push(
+  name = "do work",
+  command = as.character(Sys.info()["nodename"])
+)
+controller$wait()
+task <- controller$pop()
+expect_false(task$result[[1L]] == as.character(Sys.info()["nodename"]))
+controller$terminate()
