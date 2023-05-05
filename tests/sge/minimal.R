8495908eb4f28486ba32c9bb8ea908d044a478f1
wlandau <will.landau@lilly.com>
2023-05-05 11:56:04 -0400

    Comments and manual termination

--- tests/sge/minimal.R
+++ tests/sge/minimal.R
@@ -8,11 +8,13 @@ controller <- crew_controller_sge(
   verbose = TRUE
 )
 controller$start()
-controller$push(
+controller$push( # Should see a job submission message.
   name = "do work",
   command = as.character(Sys.info()["nodename"])
 )
 controller$wait()
 task <- controller$pop()
 expect_false(task$result[[1L]] == as.character(Sys.info()["nodename"]))
+controller$launcher$terminate() # Should see a job deletion message.
+Sys.sleep(5L)
 controller$terminate()
