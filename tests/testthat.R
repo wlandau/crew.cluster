9f7e48ad7d12dd8625c605eddde12d772c9b94a7
wlandau <will.landau@gmail.com>
2023-12-10 11:47:49 -0500

    Adjust for CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
 library(crew.cluster)
 
-test_check("crew.cluster")
+# TODO: remove conditional and require crew >= 0.7.0.
+if (packageVersion("crew") > "0.6.0") {
+  test_check("crew.cluster")
+}
